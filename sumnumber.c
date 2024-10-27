7507d27f129ac39f16ece333c030ba24592b94d9
MD Sadid <sfhsadid@gmail.com>
2024-10-27 07:12:18 +0300

    Add some new update

---
+++ sumnumber.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  int num1, num2;
+  printf("Give me the Number");
+  scanf("%d",&num1);
+  scanf("%d", &num2);
+  int sum = num1 + num2;
+  printf("Sum %d",sum);
+  return 0;
+}
---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+  int age;
+  printf("Give me your age ");
+  scanf("%d", &age);
+  printf("Your are is %d",age);
+  return 0;
+}
