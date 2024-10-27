78d10fde9f0fd0a5035d2faf92f02c6c9e13e7a3
MD Sadid <sfhsadid@gmail.com>
2024-10-27 08:18:28 +0300

    array partice 2 done

---
+++ sumaverage.c
@@ -0,0 +1,52 @@
+// #include <stdio.h>
+
+// int sum(int arr[], int size);
+
+// int main(){
+//     int arr[10];
+//     printf("Welcome to Sum and Average on Array \n");
+//     for(int i = 0; i<10; i++){
+//         printf("Please enter %dth elemant", (i+1));
+//         scanf(" %d", &arr[i]);
+//     }
+//     int addition = sum(arr, 10);
+//     float avg = addition / 10;
+//     printf("The sum of the number is %d and theire are average is %2f", addition, avg);
+// }
+
+// int sum (int arr[], int size){
+//     int addition = 0;
+//     for(int i =0; i<size; i++){
+//         addition += arr[i];
+//     }
+//     return addition;
+// }
+
+#include <stdio.h>
+
+int occurrences(int arr[], int size, int element);
+
+int main(){
+    int arr[] = {3, 12, 42, 54, 2, 3, 2, 6, 42, 12};
+    printf("Welcome to finding element occurrences \n");
+
+    int result = occurrences(arr, 10, 3);
+    printf("\nThe number of occurrences of 3 %d", result);
+    result = occurrences(arr, 10, 2);
+    printf("\nThe number of occurrences of 2 %d", result);
+    result = occurrences(arr, 10, 12);
+    printf("\nThe number of occurrences of 12 %d", result);
+    result = occurrences(arr, 10, 42);
+    printf("\nThe number of occurrences of 42 %d", result);
+}
+
+int occurrences(int arr[], int size, int element){
+    int counter = 0;
+    for(int i = 0; i<size; i++){
+        if(arr[i]== element){
+            counter++;
+
+        }
+    }
+    return counter;
+}
