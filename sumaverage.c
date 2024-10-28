56a0a5d17503c37e906586430a04a04d7c425911
MD Sadid <sfhsadid@gmail.com>
2024-10-28 08:24:15 +0300

    max and min array element find done

--- sumaverage.c
+++ sumaverage.c
@@ -22,31 +22,54 @@
 //     return addition;
 // }
 
+// #include <stdio.h>
+
+// int occurrences(int arr[], int size, int element);
+
+// int main(){
+//     int arr[] = {3, 12, 42, 54, 2, 3, 2, 6, 42, 12};
+//     printf("Welcome to finding element occurrences \n");
+
+//     int result = occurrences(arr, 10, 3);
+//     printf("\nThe number of occurrences of 3 %d", result);
+//     result = occurrences(arr, 10, 2);
+//     printf("\nThe number of occurrences of 2 %d", result);
+//     result = occurrences(arr, 10, 12);
+//     printf("\nThe number of occurrences of 12 %d", result);
+//     result = occurrences(arr, 10, 42);
+//     printf("\nThe number of occurrences of 42 %d", result);
+// }
+
+// int occurrences(int arr[], int size, int element){
+//     int counter = 0;
+//     for(int i = 0; i<size; i++){
+//         if(arr[i]== element){
+//             counter++;
+
+//         }
+//     }
+//     return counter;
+// }
+
 #include <stdio.h>
 
-int occurrences(int arr[], int size, int element);
-
 int main(){
-    int arr[] = {3, 12, 42, 54, 2, 3, 2, 6, 42, 12};
-    printf("Welcome to finding element occurrences \n");
+    int arr[10];
+    printf("Welcome to max and min of array \n");
 
-    int result = occurrences(arr, 10, 3);
-    printf("\nThe number of occurrences of 3 %d", result);
-    result = occurrences(arr, 10, 2);
-    printf("\nThe number of occurrences of 2 %d", result);
-    result = occurrences(arr, 10, 12);
-    printf("\nThe number of occurrences of 12 %d", result);
-    result = occurrences(arr, 10, 42);
-    printf("\nThe number of occurrences of 42 %d", result);
-}
-
-int occurrences(int arr[], int size, int element){
-    int counter = 0;
-    for(int i = 0; i<size; i++){
-        if(arr[i]== element){
-            counter++;
-
+    for(int i = 0; i < 10; i++){
+        printf("Please enter the %d element: ", (i + 1));
+        scanf(" %d", &arr[i]);
+    }
+    int max = arr[0];
+    int min = arr[0];
+    for(int i = 1; i<10; i++){
+        if(max <arr[i]){
+            max = arr[i];
+        }
+        if(min > arr[i]){
+            min = arr[i];
         }
     }
-    return counter;
+    printf("%d max element array %d min element of array", max, min);
 }
